c6bc4c54a620cd4ca24ce8192382626e27b63b12
Farhan Khan <farhan.k2005@gmail.com>
2022-04-20 15:56:37 -0400

    ...

--- JS_Practice/Variables/index.js
+++ JS_Practice/Variables/index.js
@@ -1,10 +1,12 @@
-let firstName = 'Farhan', lastName = "Khan";
+let firstName = "Farhan",
+  lastName = "Khan";
 let age = 17;
 let isStudent = true;
 
-console.log(firstName + " " + lastName + " is " + age + " years old"); 
+console.log(firstName + " " + lastName + " is " + age + " years old");
 console.log("Is student: " + isStudent);
 
-document.getElementById("p1").innerHTML = "Hello " + firstName + ", " + lastName;
+document.getElementById("p1").innerHTML =
+  "Hello " + firstName + ", " + lastName;
 document.getElementById("p2").innerHTML = "You are " + age + " years old";
 document.getElementById("p3").innerHTML = "Are you a student?: " + isStudent;
