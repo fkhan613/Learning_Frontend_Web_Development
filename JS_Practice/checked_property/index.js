1ca3ba1de763488191897d79f7299824894e06f1
Farhan Khan <farhan.k2005@gmail.com>
2022-04-23 15:54:15 -0400

    created checkboxes for payment methods

--- JS_Practice/checked_property/index.js
+++ JS_Practice/checked_property/index.js
@@ -1,8 +1,19 @@
 document.getElementById("submit").onclick = function () {
   const checked = document.getElementById("checkbox").checked;
+  const visa = document.getElementById("visaBtn").checked;
+  const mastercard = document.getElementById("mastercardBtn").checked;
+  const paypal = document.getElementById("paypalBtn").checked;
 
   if (checked) {
-    alert("YOU SUBSCRIBED!");
+    if (visa) {
+      alert("YOU SUBSCRIBED WITH VISA");
+    } else if (mastercard) {
+      alert("YOU SUBSCRIBED WITH MASTERCARD");
+    } else if (paypal) {
+      alert("YOU SUBSCRIBED WITH PAYPAL");
+    } else {
+      alert("Please select a payment method to continue");
+    }
   } else {
     alert("YOU DID NOT SUBSCRIBE!");
   }
