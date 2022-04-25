f94d7297e70825796b56f0e6ea7767d8718a075d
Farhan Khan <farhan.k2005@gmail.com>
2022-04-25 18:40:51 -0400

    added breaka nd continue files

---
+++ JS_Practice/break_continue_statements/index.js
@@ -0,0 +1,15 @@
+console.log("Continue statement | Continue if i == 13");
+for (let i = 1; i <= 20; i++) {
+  if (i == 13) {
+    continue;
+  }
+  console.log(i);
+}
+
+console.log("Break statement | Break if i == 13");
+for (let i = 1; i <= 20; i++) {
+  if (i == 13) {
+    break;
+  }
+  console.log(i);
+}
