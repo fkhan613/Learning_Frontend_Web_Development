09c3eda50c6a0de186112ceb95a03ed17210dab9
Farhan Khan <farhan.k2005@gmail.com>
2022-05-17 17:13:37 -0400

    added notificatioons depending on guess

--- JS_Practice/number_guessing_game/index.js
+++ JS_Practice/number_guessing_game/index.js
@@ -1,9 +1,12 @@
-const answer = Math.floor(Math.random() * 50 + 1);
+const answer = 5; //Math.floor(Math.random() * 50 + 1);
 
 document.getElementById("submit").onclick = function () {
   let guess = document.getElementById("guess").value;
-  if (guess == answer) {
+  if (guess == 0) {
+    alert("Please enter a number");
+  } else if (guess == answer) {
     alert("You got it!");
+    location.href = "https://www.youtube.com/watch?v=iik25wqIuFo";
   } else if (guess < answer) {
     alert("Too low!");
   } else if (guess > answer) {
