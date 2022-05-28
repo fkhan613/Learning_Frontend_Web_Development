e5764dc1be995dd343faa20f58b12d00343698a8
Farhan Khan <farhan.k2005@gmail.com>
2022-05-28 16:43:13 -0400

    updated conditional statement to ternery

--- JS_Practice/FizzBuzz/index.js
+++ JS_Practice/FizzBuzz/index.js
@@ -1,13 +1,11 @@
 //this prgram will iterate through a range of numbers and print fizz if the number is divisible by 3, buzz if the number is divisible by 5, and fizzbuzz if the number is divisible by both 3 and 5.
 
-for(let i = 1; i <=Math.random() * 500 + 1; i++){
-    if(i % 3 === 0 && i % 5 === 0){
-        console.log(i + ": FizzBuzz");
-    }else if(i % 3 === 0){
-        console.log(i + ": Fizz");
-    }else if(i % 5 === 0){
-        console.log(i + ": Buzz");
-    }else{
-        console.log(i + ": Nothing");
-    }
+for (let i = 1; i <= Math.random() * 500 + 1; i++) {
+  i % 3 === 0 && i % 5 === 0
+    ? console.log(i + ": FizzBuzz")
+    : i % 3 === 0
+    ? console.log(i + ": Fizz")
+    : i % 5 === 0
+    ? console.log(i + ": Buzz")
+    : console.log(i + ": Nothing");
 }
