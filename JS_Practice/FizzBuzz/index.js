3c7e055793f9f8dace04861c5779746dc73110d4
Farhan Khan <farhan.k2005@gmail.com>
2022-05-28 16:35:38 -0400

    cretaed a fizzbuzz program

---
+++ JS_Practice/FizzBuzz/index.js
@@ -0,0 +1,13 @@
+//this prgram will iterate through a range of numbers and print fizz if the number is divisible by 3, buzz if the number is divisible by 5, and fizzbuzz if the number is divisible by both 3 and 5.
+
+for(let i = 1; i <=100; i++){
+    if(i % 3 === 0 && i % 5 === 0){
+        console.log(i + ": FizzBuzz");
+    }else if(i % 3 === 0){
+        console.log(i + ": Fizz");
+    }else if(i % 5 === 0){
+        console.log(i + ": Buzz");
+    }else{
+        console.log(i + ": Nothing");
+    }
+}
