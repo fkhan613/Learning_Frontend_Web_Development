1647651ea68f16af05faaf1cec71b77d523c4b8e
Farhan Khan <farhan.k2005@gmail.com>
2022-05-28 16:39:46 -0400

    *added* random amount of numbers get generated

--- JS_Practice/FizzBuzz/index.js
+++ JS_Practice/FizzBuzz/index.js
@@ -1,6 +1,6 @@
 //this prgram will iterate through a range of numbers and print fizz if the number is divisible by 3, buzz if the number is divisible by 5, and fizzbuzz if the number is divisible by both 3 and 5.
 
-for(let i = 1; i <=100; i++){
+for(let i = 1; i <=Math.random() * 500 + 1; i++){
     if(i % 3 === 0 && i % 5 === 0){
         console.log(i + ": FizzBuzz");
     }else if(i % 3 === 0){
